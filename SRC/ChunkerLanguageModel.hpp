24d3920e9deac0c9382836776a67b9c9902f1ffa
npoe <n.poerner@campus.lmu.de>
2017-08-10 14:59:29 +0200

    started doxygenizing ChunkerLanguageModel

--- SRC/ChunkerLanguageModel.hpp
+++ SRC/ChunkerLanguageModel.hpp
@@ -1,70 +1,146 @@
 #ifndef CHUNKER_LANGUAGEMODEL_HPP
 #define CHUNKER_LANGUAGEMODEL_HPP
 
-
+/**
+ * \class Language Model
+ *
+ * \brief Base class for all language models
+ */
 class LanguageModel
 {
 protected:
-	std::deque<std::tuple<unsigned, std::string> > _nodes;
-	std::deque<std::tuple<unsigned, unsigned, std::string> > _links; 
+	std::deque<std::tuple<unsigned, std::string> > _nodes; /** List of all nodes */
+	std::deque<std::tuple<unsigned, unsigned, std::string> > _links; /** List of all links beteen nodes */
 
-	std::unique_ptr<UnitModeller> _modeller;
+	std::unique_ptr<UnitModeller> _modeller; /** Modeller being used to model types / phonemes */
 public:
-	void addNode(unsigned, const std::string &);
+	/**
+	 * \brief Add new node to language model
+	 *
+	 * \param idx Node index in language model
+	 * \param label Node's label (must be in left column of DICT)
+	 */
+	void addNode(unsigned, const std::string &); 
+	
+	/**
+	 * \brief Add new link to language model
+	 *
+	 * \param from Source node index
+	 * \param to Target node index
+	 * \param prob Link probability, as returned by wordvar (or empty)
+	 */
 	void addLink(unsigned, unsigned, std::string);
 
+	/**
+	 * \brief Print language model as HTK lattice
+	 *
+	 * \param stream Write-handle to lattice file
+	 */
 	void print(std::ostream &) const;
 
+	/**
+	 * \brief Constructor
+	 *
+	 * \param modeller Unit modeller that will be used to model types / phonemes
+	 */
 	LanguageModel(std::unique_ptr<UnitModeller>);
 };
 
+/**
+ * \class LMHTK
+ * \brief Language model that uses HTK's inbuilt HLStats and HBuild functions
+ * \deprecated
+ */
 class LMHTK : public LanguageModel
 {
 public:
 	LMHTK(std::unique_ptr<TranscriptionIterator>, std::unique_ptr<UnitModeller>, const std::string &);
 };
 
+/**
+ * \class LMZerogram
+ * \brief Simple cyclical language model that gives equal probability to all units (types / phonemes)
+ *
+ * Currently used as default for phoneme-based language models
+ */
 class LMZerogram : public LanguageModel
 {
 public:
 	LMZerogram(std::unique_ptr<TranscriptionIterator>, std::unique_ptr<UnitModeller>);
 };
 
+/**
+ * \class LMUnigram
+ * \brief Simple cyclical language model that gives units a probability that is proportionat to their unigram probabilities
+ * \deprecated
+ */
 class LMUnigram : public LanguageModel
 {
 public:
 	LMUnigram(std::unique_ptr<TranscriptionIterator>, std::unique_ptr<UnitModeller>);
 };
 
+/**
+ * \class LMBigram
+ * \brief Cyclical language model that gives units a probability that is proportionat to their smoothed bigram probabilities
+ *
+ * Currently used as default for type-based language models
+ */
 class LMBigram : public LanguageModel
 {
 public:
 	LMBigram(std::unique_ptr<TranscriptionIterator>, std::unique_ptr<UnitModeller>);
 };
 
+/**
+ * \class LMPair
+ * \brief Cyclical language model featuring unit pairs
+ * \deprecated
+ */
 class LMPair : public LanguageModel
 {
 public:
 	LMPair(std::unique_ptr<TranscriptionIterator>, std::unique_ptr<UnitModeller>);
 };
 
+/**
+ * \class LMTriple
+ * \brief Cyclical language model featuring unit triples
+ * \deprecated
+ *
+ * Slow!
+ */
 class LMTriple : public LanguageModel
 {
 public:
 	LMTriple(std::unique_ptr<TranscriptionIterator>, std::unique_ptr<UnitModeller>);
 };
 
+/**
+ * \class LMFactorAutomaton
+ * \brief Factor automaton language model. Used for forced chunking.
+ */
 class LMFactorAutomaton : public LanguageModel
 {
 public:
 	LMFactorAutomaton(std::unique_ptr<TranscriptionIterator>, std::unique_ptr<UnitModeller>);
 };
 
+/**
+ * \class LMForcedAlign
+ * \brief Alignment language model (equivalent to what MAUS is doing). Used in the last phase of forced chunking.
+ */
 class LMForcedAlign : public LanguageModel
 {
 public:
 	LMForcedAlign(std::unique_ptr<TranscriptionIterator>, std::unique_ptr<UnitModeller>);
 };
 
+/**
+ * \brief Print a given language model as HTK lattice.
+ *
+ * \param stream Write-stream to HTK lattice file
+ * \param lm Language model to be printed
+ */
 std::ostream & operator << (std::ostream &, const LanguageModel &);
 #endif
